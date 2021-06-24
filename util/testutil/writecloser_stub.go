e76d3529f34b3f6a4ee2c058a8d5e9113568d10c
Kuniwak <yuki.kokubun@dena.com>
2021-06-24 15:57:29 +0900

    Unify stub convention

--- util/testutil/writecloser_stub.go
+++ util/testutil/writecloser_stub.go
@@ -4,8 +4,27 @@ import (
 	"io"
 )
 
-func StubWriteCloser() io.WriteCloser {
-	return &NullWriteCloser{}
+type WriteCloserStub struct {
+	WriteErr error
+	CloseErr error
+}
+
+func (w WriteCloserStub) Write(p []byte) (n int, err error) {
+	if w.WriteErr == nil {
+		return len(p), nil
+	}
+	return 0, w.WriteErr
+}
+
+func (w WriteCloserStub) Close() error {
+	return w.CloseErr
+}
+
+func StubWriteCloser(writeErr, closeErr error) io.WriteCloser {
+	return &WriteCloserStub{
+		WriteErr: writeErr,
+		CloseErr: closeErr,
+	}
 }
 
 type NullWriteCloser struct{}
