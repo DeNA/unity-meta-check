6113f332057e346d60d3a449f1774a26e108478f
nils måsén <nils@piksel.se>
2021-07-05 12:00:07 +0200

    add MonoImporterGen

---
+++ unity/meta/monoimportergen.go
@@ -0,0 +1,32 @@
+
+package meta
+
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+type MonoImporterGen struct {
+	GUID *GUID
+}
+
+var _ Gen = MonoImporterGen{}
+
+func (t MonoImporterGen) WriteTo(writer io.Writer) (int64, error) {
+	content := strings.TrimLeft(fmt.Sprintf(`
+fileFormatVersion: 2
+guid: %s
+TextScriptImporter:
+  externalObjects: {}
+  userData: 
+  assetBundleName: 
+  assetBundleVariant: 
+  serializedVersion: 2
+  defaultReferences: []
+  executionOrder: 0
+  icon: {instanceID: 0}
+`, t.GUID.String()), "\n")
+	n, err := io.WriteString(writer, content)
+	return int64(n), err
+}
