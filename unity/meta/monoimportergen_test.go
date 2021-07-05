c80d478f30edfa3c5be386511bbe7acfc042e26b
nils måsén <nils@piksel.se>
2021-07-05 12:50:59 +0200

    add test

---
+++ unity/meta/monoimportergen_test.go
@@ -0,0 +1,37 @@
+package meta_test
+
+import (
+	"bytes"
+	"github.com/DeNA/unity-meta-check/unity/meta"
+	"github.com/google/go-cmp/cmp"
+	"testing"
+)
+
+func TestTextScriptImporterGen_WriteTo(t *testing.T) {
+	gen := meta.MonoImporterGen{GUID: meta.ZeroGUID()}
+
+	buf := &bytes.Buffer{}
+	_, err := gen.WriteTo(buf)
+	if err != nil {
+		t.Errorf("want nil, got %#v", err)
+		return
+	}
+
+	actual := buf.String()
+	expected := `fileFormatVersion: 2
+guid: 00000000000000000000000000000000
+TextScriptImporter:
+  externalObjects: {}
+  userData: 
+  assetBundleName: 
+  assetBundleVariant: 
+  serializedVersion: 2
+  defaultReferences: []
+  executionOrder: 0
+  icon: {instanceID: 0}
+`
+	if actual != expected {
+		t.Error(cmp.Diff(expected, actual))
+		return
+	}
+}
